82138655f26306bafaec1ed6ad59082313350c76
Scarlet Gabriella <scarletgabriella0@gmail.com>
2021-10-16 02:37:35 -0300

    update folder Dindin for Dindin.Web and add Dindin

---
+++ backend/Dindin/Dindin/Inteface/IRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Dindin.Interface
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Listar();
+        T RetornaPorTitulo(string titulo);
+        bool Inserir(T objeto);
+        bool Atualizar(string titulo, T objeto);
+        bool Excluir(string titulo);
+    }
+}
