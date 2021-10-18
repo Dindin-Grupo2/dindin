bc9e6861d9358ec76709b99c8ed62fc6a907e829
Scarlet Gabriella <scarletgabriella0@gmail.com>
2021-10-18 20:07:52 -0300

    create inteface

---
+++ backend/Dindin/Dindin/Inteface/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Dindin.Model;
+using System.Collections.Generic;
+
+namespace Dindin.Inteface
+{
+    public interface IRepositorio
+    {
+        public List<Curso> GetCursos();
+        public Curso GetCursoID(int id);
+        public List<Aula> GetAulasByCursoID(int id);
+        public bool CreateCurso(Curso curso);
+        public bool CreateAulaByCursoTitulo(string titulo, List<Aula> listaAula);
+        public bool UpdateCurso(int id, Curso curso);
+        public bool UpdateAulasByCursoTitulo(string titulo, string nomeAula, List<Aula> listaAula);
+        public bool DeleteCurso(int id);
+        public bool DeleteAulaByCursoID(int id, string tituloAula);
+    }
+}
