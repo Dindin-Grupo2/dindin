9465ac3a36ac78c411838ab28760dfa773621462
scarletgabriella <76235983+scarletgabriella@users.noreply.github.com>
2021-10-21 11:47:15 -0300

    update get.js

--- frontend/src/service/get.js
+++ frontend/src/service/get.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export default {
     getCursos() {
-        return api.GetCursos('/api/Curso').then(res => {
+        return api.get('/api/Curso').then(res => {
             return res
 
         }).catch(error => {
@@ -10,14 +10,14 @@ export default {
         })
     },
     getCursoID(IdCurso) {
-        return api.GetCursoID(`/api/Curso/${IdCurso}`).then(res => {
+        return api.get(`/api/Curso/${IdCurso}`).then(res => {
             return res
         }).catch(error => {
             return error
         })
     },
     getAulas(IdCurso) {
-        return api.GetAulasByCursoID(`/api/Curso/AulasDoCurso/${IdCurso}`).then(res => {
+        return api.get(`/api/Curso/AulasDoCurso/${IdCurso}`).then(res => {
             return res
 
         }).catch(error => {
