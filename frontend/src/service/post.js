65f7555b6811c11d74b62322c3f3c8a0f9b26f1a
scarletgabriella <76235983+scarletgabriella@users.noreply.github.com>
2021-10-21 11:47:51 -0300

    update post.js

--- frontend/src/service/post.js
+++ frontend/src/service/post.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export default {
     postCurso() {
-        return api.CreateCurso(`/api/Curso/`).then(res => {
+        return api.post(`/api/Curso/`).then(res => {
             return res
 
         }).catch(error => {
@@ -10,7 +10,7 @@ export default {
         })
     },
     postAulas(tituloDoCurso){
-        return api.CreateAulaByCursoTitulo(`/api/Curso/AulaDoCurso?titulo=${tituloDoCurso}`).then(res => {
+        return api.post(`/api/Curso/AulaDoCurso?titulo=${tituloDoCurso}`).then(res => {
             return res
 
         }).catch(error => {
